ab0863ab709d0c205159fb82fb1c89f5e5852432
Pierre <pierrelidem@gmail.com>
2023-03-14 13:46:44 +0100

    fonction search/ modification html

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ let pokeNumber = document.getElementById("id-screen");
 let type = document.getElementById("type-screen");
 let aboutScreen = document.getElementById("about-screen");
 
+let form = document.querySelector(".form");
+
+let search = document.getElementById("search");
+
 const changePokemon = async () => {
-  let randomNumber = Math.ceil(Math.random() * 150) + 1;
+  let randomNumber = Math.ceil(Math.random() * 649) + 1;
 
   let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
 
@@ -16,8 +20,11 @@ const changePokemon = async () => {
 
   let response = await data.json();
 
-  console.log(response);
-  image.src = response.sprites.front_default;
+  //console.log(response);
+  image.src =
+    response["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ];
   pokeName.innerHTML = response.name;
   pokeNumber.innerHTML = `# ${response.id}`;
   type.textContent = response.types[0].type.name;
@@ -34,7 +41,10 @@ const shiny = async () => {
   let data = await fetch(requestString);
 
   let response = await data.json();
-  image.src = response.sprites.front_shiny;
+  image.src =
+    response["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_shiny"
+    ];
   pokeName.innerHTML = response.name;
   pokeNumber.innerHTML = `# ${response.id}`;
   type.textContent = response.types[0].type.name;
@@ -42,6 +52,52 @@ const shiny = async () => {
     response.weight / 10
   }kg`;
 };
+
+/**SEARCH BUTTOM */
+
+const fetchPokemon = async (pokemon) => {
+  const APIresponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
+  );
+
+  if (APIresponse.status === 200) {
+    const data = await APIresponse.json();
+    return data;
+  }
+};
+
+const renderPokemon = async (pokemon) => {
+  pokeName.innerHTML = "Loading... ";
+  pokeNumber.innerHTML = "Loading... ";
+  type.textContent = "Loading... ";
+  aboutScreen.innerHTML = "Loading... ";
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    image.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+    pokeName.innerHTML = data.name;
+    pokeNumber.innerHTML = `# ${data.id}`;
+    type.textContent = data.types[0].type.name;
+    aboutScreen.innerHTML = `Height: ${data.height * 10}cm Weight: ${
+      data.weight / 10
+    }kg`;
+    console.log(data);
+  } else {
+    pokeName.innerHTML = "Not found";
+    pokeNumber.innerHTML = `Not found`;
+    type.textContent = "Not found";
+    aboutScreen.innerHTML = `Not found `;
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(search.value);
+  renderPokemon(search.value);
+});
 
 changePokemon();
 
